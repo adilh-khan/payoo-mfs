ffb08ef4c9b431efc8212db7fd1ef9adb143c186
Adil Hussain Khan <adilh_khan@Adils-MacBook-Pro.local>
2024-09-18 22:49:39 -0400

    added a home page

--- javaScript/login.js
+++ javaScript/login.js
@@ -1,11 +1,21 @@
 // Step-1: Set Event Handler
+// Step-2: Prevent default behaviour (prevent loading browser)
+// Step-3: Get the phone number
+// Step-4: Validate Phone Number & PIN
+// This is temporary, we will not validate information like that
 document.getElementById('button-login')
-.addEventListener('click', function(event){
-    // Step-2: Prevent default behaviour (prevent loading browser)
-    event.preventDefault();
-    console.log('Login Button Click');
-    // Step-3: Get the phone number
-    const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-})
+    .addEventListener('click', function(event){
+        event.preventDefault();
+        console.log('Login Button Click');   
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(phoneNumber, pinNumber);  
+        if(phoneNumber === '5' && pinNumber === '1234'){
+            console.log('You are logged in');
+            window.location.href = '/home.html';
+        }
+        else{
+            alert('Wrong Phone Number Or PIN')
+        }
+    })
 
